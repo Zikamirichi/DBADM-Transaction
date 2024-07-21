a7e3ca0d06bc23ed1572ef74c0f185eaf8ef34c8
Zikamirichi <bittercandy30@gmail.com@website.com>
2024-07-21 13:33:29 +0800

    added for employees module functions

--- updatedScript.sql
+++ updatedScript.sql
@@ -1,16 +1,18 @@
 -- For Employees Module
 
 -- createEmployee function in java
-USE `dbsalesv2.6`;
+-- add_employee procedure
+-- employees BEFORE INSERT trigger
+USE `DBSALES26_G208`;
 DROP procedure IF EXISTS `add_employee`;
 
-USE `dbsalesv2.6`;
-DROP procedure IF EXISTS `dbsalesv2.6`.`add_employee`;
+USE `DBSALES26_G208`;
+DROP procedure IF EXISTS `DBSALES26_G208`.`add_employee`;
 ;
 
 DELIMITER $$
-USE `dbsalesv2.6`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `add_employee`(
+USE `DBSALES26_G208`$$
+CREATE DEFINER=`DBADM_208`@`%` PROCEDURE `add_employee`(
 	IN p_lastName varchar(50), 
 	IN p_firstName varchar(50), 
 	IN p_extension varchar(10), 
@@ -24,7 +26,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `add_employee`(
 BEGIN
 	DECLARE employee_type ENUM('S','N');
     DECLARE employeeNumber1 INT;
-    DECLARE dummy INT;
+    DECLARE forlock INT;
 	
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
 	BEGIN
@@ -46,7 +48,7 @@ BEGIN
 		INSERT INTO employees (lastName, firstName, extension, email, jobTitle, employee_type, end_username, end_userreason)
 		VALUES (p_lastName, p_firstName, p_extension, p_email, p_jobTitle, employee_type, p_end_username, p_end_userreason);
 		
-        SELECT COUNT(1) INTO dummy FROM employees FOR UPDATE;
+        SELECT COUNT(1) INTO forLock FROM employees FOR UPDATE;
 		DO SLEEP(15);
 		SELECT MAX(employeeNumber) INTO employeeNumber1 FROM employees;
 		IF p_employee_Type = 'Sales Representatives'  THEN
@@ -66,4 +68,87 @@ END$$
 
 DELIMITER ;
 ;
+
+DROP TRIGGER IF EXISTS `DBSALES26_G208`.`employees_BEFORE_INSERT`;
+
+DELIMITER $$
+USE `DBSALES26_G208`$$
+CREATE DEFINER=`DBADM_208`@`%` TRIGGER `employees_BEFORE_INSERT` BEFORE INSERT ON `employees` FOR EACH ROW BEGIN
+	-- System Generated Identifier
+    DECLARE current_year CHAR(4);
+    DECLARE fixed_part INT;
+    DECLARE lastNumber  INT;
+    DECLARE NumberString VARCHAR(255);
+    DECLARE lastThreeChars VARCHAR(3);
+    
+    DECLARE forLock INT;
+    DECLARE newemployeeNumber INT; 
+    
+    SET current_year = DATE_FORMAT(CURDATE(), '%Y');
+    SET fixed_part = '02';
+	
+    SELECT COUNT(1) INTO forLock FROM employees_audit FOR UPDATE;
+    DO SLEEP(15);
+	SELECT MAX(employeeNumber) INTO lastNumber FROM employees_audit;
+    
+    SET NumberString = CAST(lastNumber AS CHAR);
+    SET lastThreeChars = RIGHT(NumberString, 3);
+    
+    SET newemployeeNumber = CAST(CONCAT(current_year, fixed_part, lastThreeChars) AS UNSIGNED) + 1;
+	IF (lastNumber is NULL) THEN
+		SET newemployeeNumber := CAST(CONCAT(current_year, fixed_part, '001') AS UNSIGNED);
+	END IF;
+		SET new.employeeNumber := newemployeeNumber;
+        
+	SET new.is_deactivated := 0;
+END$$
+DELIMITER ;
+
+
+-- for reclassifyEmployee function in java
+
+-- for resignEmployee function in java
+USE `DBSALES26_G208`;
+DROP procedure IF EXISTS `deactivateEmployee`;
+
+USE `DBSALES26_G208`;
+DROP procedure IF EXISTS `DBSALES26_G208`.`deactivateEmployee`;
+;
+
+DELIMITER $$
+USE `DBSALES26_G208`$$
+CREATE DEFINER=`DBADM_208`@`%` PROCEDURE `deactivateEmployee`(
+	IN p_employeeNumber INT,
+	IN p_end_username varchar(45),
+	IN p_end_userreason varchar(45)
+)
+BEGIN
+	DECLARE forLock INT;
+	SELECT COUNT(1) INTO forLock FROM employees FOR UPDATE;
+	DO SLEEP(15);
+	UPDATE employees SET is_deactivated = 1, end_username = p_end_username, end_userreason = p_end_userreason where employeeNumber = p_employeeNumber;
+    
+    DELETE FROM inventory_managers WHERE employeeNumber= p_employeeNumber;
+	DELETE FROM sales_managers WHERE employeeNumber= p_employeeNumber;
+	DELETE FROM Non_SalesRepresentatives WHERE employeeNumber= p_employeeNumber;
+    DELETE FROM salesRepresentatives WHERE employeeNumber= p_employeeNumber;
+    
+    SELECT COUNT(1) INTO forLock FROM salesRepAssignments FOR UPDATE;
+	DO SLEEP(15);
+    UPDATE salesRepAssignments SET endDate = CURDATE(), end_username = p_end_username, end_userreason = p_end_userreason
+	WHERE employeeNumber = p_employeeNumber AND NOW() >= startDate AND NOW() <= endDate;
+END$$
+
+DELIMITER ;
+;
+
+-- for (insert function) in java
+
+-- For Product Management Module
+
+-- For Sales Order Module
+
+
+
+
 
