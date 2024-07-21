63e62fd5d7e3901138959f54fb4b5717efa47935
s22a55m77 <s22a55m77@gmail.com>
2024-07-21 20:41:11 +0800

    remove sleep in deactivateEmployee

--- updatedScript.sql
+++ updatedScript.sql
@@ -413,7 +413,6 @@ CREATE DEFINER=`DBADM_208`@`%` PROCEDURE `deactivateEmployee`(
 BEGIN
 	DECLARE forLock INT;
 	SELECT COUNT(1) INTO forLock FROM employees FOR UPDATE;
-	DO SLEEP(15);
 	UPDATE employees SET is_deactivated = 1, end_username = p_end_username, end_userreason = p_end_userreason where employeeNumber = p_employeeNumber;
     
     DELETE FROM inventory_managers WHERE employeeNumber= p_employeeNumber;
@@ -422,7 +421,6 @@ BEGIN
     DELETE FROM salesRepresentatives WHERE employeeNumber= p_employeeNumber;
     
     SELECT COUNT(1) INTO forLock FROM salesRepAssignments FOR UPDATE;
-	DO SLEEP(15);
     UPDATE salesRepAssignments SET endDate = CURDATE(), end_username = p_end_username, end_userreason = p_end_userreason
 	WHERE employeeNumber = p_employeeNumber AND NOW() >= startDate AND NOW() <= endDate;
 END$$
